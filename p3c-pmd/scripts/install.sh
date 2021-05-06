857eb30e70915796d030b906c59b42b8278f077c
iamharvey <39893271+iamharvey@users.noreply.github.com>
2021-05-06 10:51:02 +0800

    add installer and precommit support for windows

--- p3c-pmd/scripts/install.sh
+++ p3c-pmd/scripts/install.sh
@@ -8,6 +8,26 @@
 target_path="${HOME}/juphoon/dev_tools"
 jar="juphoon-p3c-pmd-1.0.0-jar-with-dependencies.jar"
 
+if type -p java; then
+    echo "检测到 Java Executable."
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo "检测到 Java Executable, JAVA_HOME=$JAVA_HOME"
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "未检测到 Java Executable, 请确保安装了Java SRE."
+    exit 1
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo "检测到版本 $version ..."
+    if [[ "$version" < "1.7" ]]; then
+        echo "版本太低，请将JAVA版本升级到1.7+"
+        exit 1
+    fi
+fi
+
 if [ ! -d ${target_path} ]
 then
     mkdir -p ${target_path}
@@ -15,7 +35,7 @@ fi
 
 if [ ! -e ${jar} ]
 then
-    echo "p3c-pmd jar file is not found! Please download the installer from github.com/juphoon/p3c release section"
+    echo "p3c-pmd jar file 未找到! 请下载installer ==> github.com/juphoon/p3c release section."
     exit 1
 fi
 
